b78502254a274b0a0462eade4b871b4718a3c404
Flavio Catuara <f.catuara@outlook.it>
2024-07-25 12:21:35 +0200

    feat: some updates

--- app/src/main/java/com/example/bizcardcomposecourse/ui/theme/Theme.kt
+++ app/src/main/java/com/example/bizcardcomposecourse/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.example.bizcardcomposecourse.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
