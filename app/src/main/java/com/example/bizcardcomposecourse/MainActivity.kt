fb5cd5347792e8e4057703500e5093f2c089f7a5
Flavio Catuara <f.catuara@outlook.it>
2025-08-05 18:02:42 +0200

    feat: add Hilt entry point

--- app/src/main/java/com/example/bizcardcomposecourse/MainActivity.kt
+++ app/src/main/java/com/example/bizcardcomposecourse/MainActivity.kt
@@ -14,7 +14,9 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.bizcardcomposecourse.noteapp.model.Note
 import com.example.bizcardcomposecourse.noteapp.screen.NoteHomeScreen
 import com.example.bizcardcomposecourse.noteapp.screen.NoteViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
