e27dfa76d312f03ac1cd3d3143514f9a4a4bf828
Flavio Catuara <f.catuara@outlook.it>
2025-08-05 18:03:07 +0200

    feat: add room database

--- app/src/main/java/com/example/bizcardcomposecourse/noteapp/model/Note.kt
+++ app/src/main/java/com/example/bizcardcomposecourse/noteapp/model/Note.kt
@@ -1,7 +1,13 @@
 package com.example.bizcardcomposecourse.noteapp.model
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.time.Instant
 import java.time.LocalDateTime
+import java.util.Date
 import java.util.UUID
+import kotlin.time.ExperimentalTime
 
 /**
  * @property id Unique identifier for the note. Automatically generated using UUID.randomUUID()
@@ -9,9 +15,15 @@ import java.util.UUID
  * @property entryDate Creation timestamp of the note. Automatically set to the current date and time
  *                     using LocalDateTime.now() if not provided.
  */
+@Entity(tableName = "notes_tbl")
+@OptIn(ExperimentalTime::class)
 data class Note(
+    @PrimaryKey
     val id: UUID = UUID.randomUUID(),
+    @ColumnInfo(name = "note_title")
     val title: String,
+    @ColumnInfo(name = "note_description")
     val description: String,
-    val entryDate: LocalDateTime = LocalDateTime.now()
+    @ColumnInfo(name = "note_entry_date")
+    val entryDate: Date = Date.from(Instant.now())
 )
